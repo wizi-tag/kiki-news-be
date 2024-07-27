acfbf63eb2714e7f7a940434bdbee21b2727ea5c
NKrasnikov <a.kaplyash@gmail.com>
2024-07-28 01:38:36 +0800

    db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASS'),
+        host: configService.get('DB_HOST'),
+        database: configService.get('DB_NAME'),
+        port: configService.get('DB_PORT'),
+        dialect: configService.get('DB_DIALECT'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
